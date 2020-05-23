a2b52d379faacba55fc38f43b28521dfe82e6aa3
AntonKozmod <antoxa.kozmod@gmail.com>
2020-05-23 06:13:50 +0000

    Определение схемы

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ var express = require("express"),
 	toDos = [
 		// настраиваем список задач копированием
 		// содержимого из файла todos.OLD.json
-	];
+	],
+	// импортируем библиотеку mongoose
+	mongoose = require("mongoose");
 app.use(express.static(__dirname + "/client"));
+// Это модель Mongoose для задач
+var ToDoSchema = mongoose.Schema({
+	description: String,
+	tags: [ String ]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
 http.createServer(app).listen(3000);
 // Этот маршрут замещает наш файл
 // todos.json в примере из части 5
 app.get("/todos.json", function (req, res) {
-	res.json(toDos);
+	ToDo.find({}, function (err, toDos) {
+		// не забудьте о проверке на ошибки
+		res.json(toDos);
+	});
 });
 // командуем Express принять поступающие
 // объекты JSON
 app.use(express.urlencoded({ extended: true }));
+// подключаемся к хранилищу данных Amazeriffic в Mongo
+mongoose.connect('mongodb://localhost/amazeriffic');
+
 app.post("/todos", function (req, res) {
 	// сейчас объект сохраняется в req.body
 	var newToDo = req.body;
