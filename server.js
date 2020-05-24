c25528ec6812ea615c369f0b0dfb1095596e083d
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-05-24 21:48:57 +0300

    Перемещение функций todos в специальный модуль

--- server.js
+++ server.js
@@ -2,19 +2,12 @@ var express = require("express"),
 	http = require("http"),
 	app = express(),
 	mongoose = require("mongoose"),
-	ToDo = require("./models/todo.js");	
+	ToDosController = require("./controllers/todos_controller.js"),
+		services, mongoUrl;
 	
 app.use(express.static(__dirname + "/client"));
 // Это модель Mongoose для задач
 http.createServer(app).listen(3000);
-// Этот маршрут замещает наш файл
-// todos.json в примере из части 5
-app.get("/todos.json", function (req, res) {
-	ToDo.find({}, function (err, toDos) {
-		// не забудьте о проверке на ошибки
-		res.json(toDos);
-	});
-});
 // командуем Express принять поступающие
 // объекты JSON
 app.use(express.urlencoded({ extended: true }));
@@ -28,24 +21,6 @@ mongoose.connect('mongodb://localhost/amazeriffic', {
 	}).catch(err => {
 		console.log(Error, err.message);
 	});
-app.post("/todos", function (req, res) {
-	console.log(req.body);
-	var newToDo = new ToDo({"description":req.body.description,
-		"tags":req.body.tags});
-	newToDo.save(function (err, result) {
-		if (err !== null) {
-			console.log(err);
-			res.send("ERROR");
-		} else {
-			// клиент ожидает, что будут возвращены все задачи,
-			// поэтому для сохранения совместимости сделаем дополнительный запрос
-			ToDo.find({}, function (err, result) {
-				if (err !== null) {
-					// элемент не был сохранен
-					res.send("ERROR");
-				}
-				res.json(result);
-			});
-		}
-	});
-});
+
+app.get("/todos.json", ToDosController.index);
+app.post("/todos", ToDosController.create);
