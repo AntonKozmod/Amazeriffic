4c6b960da36e473ca47f51e585224f8d8afce212
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-05-26 12:27:09 +0300

    Совершенствуем действия контроллера ToDo

--- models/todo.js
+++ models/todo.js
@@ -1,8 +1,11 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"),
+	ToDoSchema,
+	ObjectId = mongoose.Schema.Types.ObjectId;
 // Это модель mongoose для списка задач
-var ToDoSchema = mongoose.Schema({
+ToDoSchema = mongoose.Schema({
 	description: String,
-	tags: [ String ]
+	tags: [ String ],
+	owner : { type: ObjectId, ref: "User" }
 });
 var ToDo = mongoose.model("ToDo", ToDoSchema);
 module.exports = ToDo;
