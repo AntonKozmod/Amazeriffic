180c135d3a878cd07bf92f40107dd610c9c89f7b
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-05-26 15:15:05 +0300

    Обновление элементов списка fix 8

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ mongoose.connect('mongodb://localhost/amazeriffic', {
 app.get("/todos.json", ToDosController.index);
 app.get("/todos/:id", ToDosController.show);
 app.post("/todos", ToDosController.create);
+app.put("/todos/:id", ToDosController.update);
+app.delete("/todos/:id", ToDosController.destroy);
 
 app.get("/user/:username/todos.json", ToDosController.index);
 app.post("/user/:username/todos", ToDosController.create);
