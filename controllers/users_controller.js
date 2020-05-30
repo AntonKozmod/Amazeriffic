e7bc0763a8635773aab42fdede8e8422e384fa4c
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-05-30 08:26:45 +0300

    fix удаления списка у удаляемого пользователя

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -91,17 +91,38 @@ UsersController.update = function (req, res) {
 UsersController.destroy = function (req, res) {
 	console.log("Вызвано действие: удалить пользователя");
 	var username = req.params.username;
-	User.deleteOne({"username": username}, function (err, user) {
-		if (err !== null) {
-			res.status(500).json(err);
-		} else {
-			if (user.n === 1 && user.ok === 1 && user.deletedCount === 1) {
-				res.status(200).json(user);
-			} else {
-				res.status(404).json({"status": 404});
-			}
-		}
+	User.find({"username": username}, function (err, result) {
+		if (err) {
+            console.log(err);
+            res.send(500, err);
+        } else if (result.length !== 0) {
+        	console.log("Удаляем все todo с 'owner': " + result[0]._id);
+        	ToDo.deleteMany({"owner": result[0]._id}, function (err, todo) {
+		        console.log("Удаляем пользователя");
+				User.deleteOne({"username": username}, function (err, user) {
+					if (err !== null) {
+						res.status(500).json(err);
+					} else {
+						if (user.n === 1 && user.ok === 1 && user.deletedCount === 1) {
+							res.status(200).json(user);
+						} else {
+							res.status(404).json({"status": 404});
+						}
+					}
+				});
+        	});
+        } else {
+            res.status(404).send("Пользователь не существует");
+            console.log(err);   
+        }
 	});
+
+
+
+
+
+
+
 }
 
 module.exports = UsersController;
